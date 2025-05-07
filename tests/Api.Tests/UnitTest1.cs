243636705e8fa7ac1773dbb7817ea685d810a1c4
Renato Golia <renato.golia@insightarchitectures.com>
2025-05-07 22:16:53 +0200

    Injected custom instance of AWSOptions with fake credentials

--- tests/Api.Tests/UnitTest1.cs
+++ tests/Api.Tests/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestPlatform.TestHost;
 
 namespace Api.Tests;
@@ -9,12 +10,21 @@ public class Tests
     [Test]
     public async Task Test1()
     {
-        WebApplicationFactory<Program> factory = new WebApplicationFactory<Program>();
+        WebApplicationFactory<Program> factory = new WebApplicationFactory<Program>()
+        .WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddDefaultAWSOptions(new Amazon.Extensions.NETCore.Setup.AWSOptions{
+                    Credentials = new Amazon.Runtime.AnonymousAWSCredentials(),
+                });
+            });
+        });
 
         var http = factory.CreateDefaultClient();
 
         var response = await http.GetAsync("/weatherforecast");
 
-        response.EnsureSuccessStatusCode();
+        Assert.That(response.IsSuccessStatusCode, Is.True, "Response was not successful");
     }
 }
